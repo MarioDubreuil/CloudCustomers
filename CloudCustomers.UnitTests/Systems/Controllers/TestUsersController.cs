25634543e99596597a918fd03711abdd549eb6aa
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-06-03 17:10:28 -0400

    test GetAllUsers invoked

--- CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -1,6 +1,9 @@
 using CloudCustomers.API.Controllers;
+using CloudCustomers.API.Models;
+using CloudCustomers.API.Services;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 
 namespace CloudCustomers.UnitTests.Systems.Controllers;
 
@@ -9,8 +12,34 @@ public class TestUsersController
     [Fact]
     public async Task Get_OnSuccess_ReturnsStatusCode200()
     {
-        var sut = new UsersController();
+        // Arrange
+        var mockUsersSservice = new Mock<IUsersService>();
+        mockUsersSservice
+            .Setup(service => service.GetAllUsers())
+            .ReturnsAsync(new List<User>());
+        var sut = new UsersController(mockUsersSservice.Object);
+
+        // Act
         var result = (OkObjectResult) await sut.Get();
+
+        // Assert
         result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_InvokesUsersServiceExactlyOnce()
+    {
+        // Arrange
+        var mockUsersSservice = new Mock<IUsersService>();
+        mockUsersSservice
+            .Setup(service => service.GetAllUsers())
+            .ReturnsAsync(new List<User>());
+        var sut = new UsersController(mockUsersSservice.Object);
+
+        // Act
+        var result = await sut.Get();
+
+        // Assert
+        mockUsersSservice.Verify(service => service.GetAllUsers(), Times.Once());
     }
 }
